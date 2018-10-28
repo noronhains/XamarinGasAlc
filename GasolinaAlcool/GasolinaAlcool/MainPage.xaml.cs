4a0907125c7d5361335436a68460108dfd91727b
Igor Noronha <igoru.noronha@gmail.com>
2018-10-28 14:46:56 -0300

    Versão básica funcional

--- GasolinaAlcool/GasolinaAlcool/MainPage.xaml.cs
+++ GasolinaAlcool/GasolinaAlcool/MainPage.xaml.cs
@@ -9,9 +9,40 @@ namespace GasolinaAlcool
 {
     public partial class MainPage : ContentPage
     {
+        const float valorDiv = 0.70f;
         public MainPage()
         {
             InitializeComponent();
+        }
+
+        private void btnVerificar_Clicked(object sender, EventArgs e)
+        {
+            float precoAlcool;
+            float precoGasolina;
+            lblExibeResultado.Text = "";
+            if (txtAlcool.Text == null || txtGasolina.Text == null || txtAlcool.Text == "" || txtGasolina.Text == "")
+            {
+                return;
+            }
+            try
+            {
+                precoAlcool = float.Parse(txtAlcool.Text);
+                precoGasolina = float.Parse(txtGasolina.Text);
+
+                if(precoAlcool / precoGasolina <= valorDiv)
+                {
+                    lblExibeResultado.Text = "Abasteça com Álcool!";
+                }
+                else
+                {
+                    lblExibeResultado.Text = "Abasteça com Gasolina!";
+                }
+                
+
+            }catch(Exception ex)
+            {
+                throw ex;
+            }
         }
     }
 }
